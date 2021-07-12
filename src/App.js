2ff5ba9f7d15da9f88441dbd87bdadb31be1c076
omkar2398 <omkargovalkar23@gmail.com>
2021-07-12 21:35:43 +0530

    Added Comments

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import Home from "./components/Home/Index";
 import Hero from "./components/Hero/Hero";
 import LinkShortner from "./components/LinkShortner/LinkShortner";
-import AdvancedStatistics from "./components/AdvancedStatistics/AdvancedStatistics";
-import Footer from "./components/Footer/Footer";
+// import AdvancedStatistics from "./components/AdvancedStatistics/AdvancedStatistics";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
